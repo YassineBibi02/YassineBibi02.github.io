cb3ff27c58645f253dad31d804648d18aa49863c
Yassine Bibi <Yassine.bibi@stud.uni-hannover.de>
2025-02-06 01:38:41 +0100

    Changed the projects Component

--- app/components/ComplicatedComponents/ProjectCard.tsx
+++ app/components/ComplicatedComponents/ProjectCard.tsx
@@ -6,26 +6,28 @@ interface ProjectCardProps {
   link: string;
   icon: string;
   bgColor: string;
-  txtColor:string;
-
+  txtColor: string;
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, link, icon, bgColor, txtColor }) => {
-
-
   return (
-    <div className={`p-6 rounded-md shadow-md ${txtColor} ${bgColor} w-full sm:w-[300px]`}>
+    <div className={`p-6 rounded-md shadow-md ${txtColor} ${bgColor} w-full sm:w-[300px] relative`}>
       <div className="flex justify-center mb-4">
         <img src={icon} alt={title} className="w-12 h-12" />
       </div>
-      <h2 className="text-xl font-bold">{title}</h2>
-      <p className="text-sm mt-2">{description}</p>
-      {link ? (
-        <a href={link} target="_blank" rel="noopener noreferrer" className="mt-4 inline-block text-sm font-semibold underline">
+      <h2 className="text-xl  font-bold">{title}</h2>
+      <p className="text-sm mt-2 mb-4">{description}</p>
+      
+      {link && (
+        <a 
+          href={link} 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className="absolute bottom-4 left-6 text-sm font-semibold underline "
+        >
           VISIT WEBSITE
         </a>
-      ) : null}
-      
+      )}
     </div>
   );
 };
